b0e8c52d2c20501b9019cba5935a653e466ae4f8
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2022-09-12 11:53:12 -0500

    Solucion Tema 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*Persona Instance*/
+        Persona persona = new Persona();
+        /*Set Persona values*/
+        persona.setNombre("Sebastián Ochoa");
+        persona.setEdad(25);
+        persona.setTelefono(995676593);
+        /*Show in screen*/
+        System.out.println("Estudiante: "+persona.getNombre());
+        System.out.println("Edad: "+ persona.getEdad());
+        System.out.println("Contacto: "+persona.getTelefono());
+
+
+
     }
 }
