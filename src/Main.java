b0df27f82f46934ad76225d083004e5811af3681
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2022-09-12 13:07:32 -0500

    Sol Ej Tema 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Cliente Instance
+        Cliente client = new Cliente();
+        //Set client values
+        client.setNombre("Sebastian Ochoa");
+        client.setEdad(25);
+        client.setTelefono(995676593);
+        client.setCredito(250);
+        //Show on screen
+        System.out.println("Cliente: "+client.getNombre()+"\n"+"Edad: "+client.getEdad()+"\n"+"Contacto: "+client.getTelefono()+"\n"+"Credito: "+client.getCredito());
+        //Employee Instance
+        Trabajador employee= new Trabajador();
+        //Set employee values
+        employee.setNombre("Alejandro Maldonado");
+        employee.setEdad(27);
+        employee.setTelefono(95686769);
+        employee.setSalario(1800);
+        //Show on screen
+        System.out.println("Trabajador: "+employee.getNombre()+"\n"+"Edad: "+employee.getEdad()+"\n"+"Contacto: "+employee.getTelefono()+"\n"+"Salario: "+employee.getSalario());
+
+
     }
 }
